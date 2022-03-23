5417fcbf5a812b5d570f09f0b91d0df404464605
dtmMartian <99682473+dtmMartian@users.noreply.github.com>
2022-03-23 14:26:29 -0700

    Added new dependencies to use APIAdded a new class to test API.

---
+++ ass3/src/main/java/nlp/SpellCheck.java
@@ -0,0 +1,29 @@
+package nlp;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+public class SpellCheck {
+    String testSentence = "thiss iss a test";
+    public static void main(String[] args) {
+        HttpResponse<String> response = Unirest.post("https://jspell-checker.p.rapidapi.com/check" + testSentence)
+                .header("X-RapidAPI-Host", "jspell-checker.p.rapidapi.com")
+                .header("X-RapidAPI-Key", "0ed8cf5c0dmsha7fcae6f8ed1076p10cd87jsn35150723b6cc")
+                .asJson();
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                JsonParser jp = new JsonParser();
+                JsonElement je = jp.parse(response.getBody().toString());
+                String prettyJsonString = gson.toJson(je);
+                System.out.println(prettyJsonString);
+                .asString();
+
+
+        }
+    }
+
+}
